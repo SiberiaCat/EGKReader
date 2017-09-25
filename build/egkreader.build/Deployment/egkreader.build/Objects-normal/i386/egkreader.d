bd999ab7c1881a4dee89ec5168bb4e96df691d08
AB <ab@procedia.de>
2017-09-25 09:50:29 +0200

    OSX: Kompilierung mit 10.10

--- build/egkreader.build/Deployment/egkreader.build/Objects-normal/i386/egkreader.d
+++ build/egkreader.build/Deployment/egkreader.build/Objects-normal/i386/egkreader.d
@@ -1,6 +1,6 @@
 dependencies: \
-  /Users/macprodev/Workbench/_Dev_Plugins/EGKPlugin/EGKPlugin_170808/egkreader.cpp \
-  /Users/macprodev/Workbench/_Dev_Plugins/EGKPlugin/EGKPlugin_170808/4D\ Plugin\ API/4DPluginAPI.h \
-  /Users/macprodev/Workbench/_Dev_Plugins/EGKPlugin/EGKPlugin_170808/4D\ Plugin\ API/Flags.h \
-  /Users/macprodev/Workbench/_Dev_Plugins/EGKPlugin/EGKPlugin_170808/4D\ Plugin\ API/PublicTypes.h \
-  /Users/macprodev/Workbench/_Dev_Plugins/EGKPlugin/EGKPlugin_170808/egkreader.h
+  /Users/macprodev/Workbench/_Dev_Plugins/EGKPlugin/EGKPlugin/egkreader.cpp \
+  /Users/macprodev/Workbench/_Dev_Plugins/EGKPlugin/EGKPlugin/4D\ Plugin\ API/4DPluginAPI.h \
+  /Users/macprodev/Workbench/_Dev_Plugins/EGKPlugin/EGKPlugin/4D\ Plugin\ API/Flags.h \
+  /Users/macprodev/Workbench/_Dev_Plugins/EGKPlugin/EGKPlugin/4D\ Plugin\ API/PublicTypes.h \
+  /Users/macprodev/Workbench/_Dev_Plugins/EGKPlugin/EGKPlugin/egkreader.h
